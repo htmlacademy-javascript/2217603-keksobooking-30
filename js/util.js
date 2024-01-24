b4c99d6b56df87ebdc220f804700190b160ba874
Margarita <balahninarita@gmail.com>
2024-01-24 20:45:31 +0300

    Инактивирует формы в разметке

--- js/util.js
+++ js/util.js
@@ -1,24 +1,5 @@
-// // Количество миллисекунд показа сообщения об ошибке
-// const DATA_ERROR_SHOW_TIME = 5000;
-
 // Задержка отрисовки фильтров
 const RERENDER_DELAY = 500;
-
-// // Шаблон сообщения об ошибке загрузки объявлений с сервера
-// const dataErrorTemplate = document.querySelector('#data-error')
-//   .content.querySelector('.data-error');
-
-// // Показывает ошибку загрузки
-// const showDataError = () => {
-//   const dataError = dataErrorTemplate.cloneNode(true);
-//   document.body.append(dataError);
-//   setTimeout(() => {
-//     dataError.remove();
-//   }, DATA_ERROR_SHOW_TIME);
-// };
-
-// // Определяет, является ли кнопка Esc
-// const isEscapeKey = (evt) => evt.key === 'Escape';
 
 // Устранение дребезга
 const debounce = (callback, timeoutDelay = RERENDER_DELAY) => {
@@ -29,4 +10,4 @@ const debounce = (callback, timeoutDelay = RERENDER_DELAY) => {
   };
 };
 
-export { /*isEscapeKey,*/ /*showDataError,*/ debounce };
+export { debounce };
